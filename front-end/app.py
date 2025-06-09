7b7dc418e8bc7c29af5b5e629687340f875d28f4
HyprPixl <HyprPixl>
2025-06-09 17:40:00 +0000

    app base working

--- front-end/app.py
+++ front-end/app.py
@@ -1,18 +1,15 @@
-import io
-import base64
-import re
-import json
 import pandas as pd
-
+import numpy as np
 import streamlit as st
-from databricks import sql
 from databricks.sdk import WorkspaceClient
-from langchain_core.messages import AIMessage, HumanMessage
-
-from graph import invoke_our_graph, sqlQuery
-from st_callable_util import get_streamlit_cb
 
 w = WorkspaceClient()
 st.set_page_config(layout="wide")
 
 st.title("hello")
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+st.map(df)
